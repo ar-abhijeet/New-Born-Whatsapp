bf2ee4ff9be9ecdf7cd4e1d0979134345abf97ce
Abhijeet Rao <abhijeetrao7800@gmail.com>
2025-06-07 02:48:59 +0530

    Added some error handlers middlewares

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = express();
 const mongoose = require('mongoose');
 const path = require("path");
 const Chat = require("./models/chat.js")
-const methodOverride = require("method-override")
+const methodOverride = require("method-override");
+const ExpressError = require("./ExpressError");
 
 app.set("views", path.join(__dirname,"views"));
 app.set("view engine","ejs");
@@ -27,10 +28,22 @@ app.get("/",(req,res)=>{
   res.send("Root is working!");
 })
 
+
+// Error Handling Middleware
+app.use((err,req,res,next)=>{
+  let {status = 500, message = "Some error occured"} = err;
+  res.status(status).send(message);
+});
+
 // Index root
-app.get("/chat",async(req,res)=>{
-  let chats = await Chat.find();
-  res.render("index.ejs",{chats});
+app.get("/chat",async(req,res,next)=>{
+  try{
+    let chats = await Chat.find();
+    res.render("index.ejs",{chats});
+  }
+  catch(err){
+    next(err);
+  }
 })
 
 //New chat route
@@ -39,37 +52,54 @@ app.get("/chat/new",(req,res)=>{
 })
 
 //Edit route
-app.get("/chat/:id/edit",async(req,res)=>{
-  let {id} = req.params;
-  let chat = await Chat.findById(id);
-  res.render("edit.ejs",{chat});
+app.get("/chat/:id/edit",async(req,res,next)=>{
+  try{
+    let {id} = req.params;
+    let chat = await Chat.findById(id);
+    res.render("edit.ejs",{chat});
+  }
+  catch(err){
+    next(err);
+  }
 })
 
 //Update route
-app.put("/chat/:id",async(req,res)=>{
-  let {id} = req.params;
-  let {msg:newmsg} = req.body;
-  let updatedChat = await Chat.findByIdAndUpdate(
+app.put("/chat/:id",async(req,res,next)=>{
+  try{
+    
+   let {id} = req.params;
+   let {msg:newmsg} = req.body;
+   let updatedChat = await Chat.findByIdAndUpdate(
     id,
     {msg:newmsg},
     {runValidators:true, new:true},
-  );
-  console.log(updatedChat);
-  res.redirect("/chat");
+   );
+   console.log(updatedChat);
+   res.redirect("/chat");
+  }
+  catch(err){
+    next(err);
+  }
 })
 
 //Destroy Route
 
-app.delete("/chat/:id",async(req,res)=>{
-  let {id} = req.params;
-  let deletedChat = await Chat.findByIdAndDelete(id);
-  console.log(deletedChat);
-  res.redirect("/chat");
+app.delete("/chat/:id",async(req,res,next)=>{
+  try{
+    let {id} = req.params;
+    let deletedChat = await Chat.findByIdAndDelete(id);
+    console.log(deletedChat);
+    res.redirect("/chat");
+  }
+  catch(err){
+    next(err);
+  }
 })
 
-app.post("/chat",(req,res)=>{
-  let {from,msg,to} = req.body;
-  let newChat = new Chat({
+app.post("/chat",(req,res,next)=>{
+  try{
+    let {from,msg,to} = req.body;
+    let newChat = new Chat({
     from:from,
     msg:msg,
     to:to,
@@ -83,6 +113,10 @@ app.post("/chat",(req,res)=>{
     console.log("Error detected",err);
   })
   res.redirect("/chat");
+  }
+  catch(err){
+    next(err);
+  }
 })
 
 app.listen(8080,()=>{
